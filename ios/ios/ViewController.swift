5e647d0188899eaf5d24bfe9806e5c94b088453d
Didia Aristote <thefuture2092@gmail.com>
2016-05-21 10:28:54 -0400

    Initial project setup for ios app

---
+++ ios/ios/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  ios
+//
+//  Created by Aristote Diasonama on 16-05-21.
+//  Copyright © 2016 Aristote Diasonama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
